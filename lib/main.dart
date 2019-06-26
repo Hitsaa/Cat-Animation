37bc28105c008de571d4b62ea978ddd6bfcf7e3a
Hitendra Singh Shekhawat <33285810+Hitsaa@users.noreply.github.com>
2019-06-26 13:31:48 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'src/app.dart';
 
-import 'package:flutter/rendering.dart';
+//import 'package:flutter/rendering.dart';
 void main() {
 
   //debugPaintSizeEnabled = true;
